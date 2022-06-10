ab0907aea2ceb7036b91877911f056587c307d8a
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-10 22:39:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -61,10 +61,10 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
+global.author = '★ꜱɪʀɪᴜꜱ★'
 
-global.wm = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.igfg = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
+global.wm = '★ꜱɪʀɪᴜꜱ★'
+global.igfg = '★ꜱɪʀɪᴜꜱ - 𝙱𝚘𝚝★'
 global.wait = '*⌛ _Wait..._ ▬▬▬▭*'
 
 global.multiplier = 9999 // The higher, The harder levelup
