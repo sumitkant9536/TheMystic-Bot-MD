2c6fb1cbc90cb7c492349ee03a8fa3362aea950d
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-10 18:02:50 +0530

    Update info-grouplist.js

--- plugins/info-grouplist.js
+++ plugins/info-grouplist.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let txt = ''
 for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n—◉ ${await conn.getName(jid)}\n➤ ${jid} [${chat?.metadata?.read_only ? '𝙽𝙾 𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴' : '𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴'}]\n\n`
-m.reply(`*𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂 𝙴𝙽 𝙻𝙾𝚂 𝚀𝚄𝙴 𝙴𝚂𝚃𝙰 𝙴𝙻 𝙱𝙾𝚃:*
+m.reply(`*𝙻𝙸𝚂𝚃 𝙾𝙵 𝙶𝚁𝙾𝚄𝙿𝚂 𝙸𝙽 𝚆𝙷𝙸𝙲𝙷 𝚃𝙷𝙴 𝙱𝙾𝚃 𝙸𝚂:*
 ${txt}
 `.trim())
 }
