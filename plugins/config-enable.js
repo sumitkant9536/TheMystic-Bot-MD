e9295e8470da5f1863dbfc419cfe6d5f21f471ac
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-11 03:12:21 +0530

    Update config-enable.js

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -214,4 +214,5 @@ conn.sendButton(m.chat, `🗂️ 𝐎𝐏𝐓𝐈𝐎𝐍: ${type}
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['group', 'owner']
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff)|[01])$/i
+handler.premium = true
 export default handler
