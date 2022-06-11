74c687c3d54c62bd5680c87c52e66a78a4bd93f8
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-11 22:13:50 +0530

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -439,7 +439,7 @@ conn.sendHydrated(m.chat, str, wm, pp, 'https://github.com/BrunoSobrino/TheMysti
 conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m)
 throw e
 }}
-handler.help = ['menu', 'help', '?']
+handler.help = ['menu', 'help']
 handler.tags = ['main']
 handler.command = /^(menucompleto|menu|menú|memu|memú|help|info|comandos|allmenu|ayuda|commands|commandos|m)$/i
 handler.exp = 50
