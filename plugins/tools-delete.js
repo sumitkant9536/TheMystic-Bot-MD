4bb702b9b7abf7a943d1d33d9379912c0eee0da4
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-11 16:13:14 +0530

    Update tools-delete.js

--- plugins/tools-delete.js
+++ plugins/tools-delete.js
@@ -2,7 +2,7 @@ let handler = function (m) {
 if (!m.quoted) throw false
 let { chat, fromMe, isBaileys } = m.quoted
 if (!fromMe) throw false
-if (!isBaileys) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚃𝙷𝙰𝚃 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 𝚆𝙰𝚂𝙽'𝚃 𝚂𝙴𝙽𝚃 𝙱𝚈 𝙼𝙴, 𝙸 𝙲𝙰𝙽'𝚃 𝙳𝙴𝙻𝙴𝚃𝙴 𝙸𝚃*'
+if (!isBaileys) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚃𝙷𝙰𝚃 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 𝚆𝙰𝚂 𝙽𝙾𝚃 𝚂𝙴𝙽𝚃 𝙱𝚈 𝙼𝙴, 𝙸 𝙲𝙰𝙽 𝙽𝙾𝚃 𝙳𝙴𝙻𝙴𝚃𝙴 𝙸𝚃*'
 conn.sendMessage(chat, { delete: m.quoted.vM.key })
 }
 handler.help = ['del', 'delete']
