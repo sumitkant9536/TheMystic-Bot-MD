89896aaaaca0ffe478f4cc69b4d1e234a8e0751c
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-11 12:02:23 +0530

    Update gc-promote.js

--- plugins/gc-promote.js
+++ plugins/gc-promote.js
@@ -8,7 +8,7 @@ var number = text
 }
 	
 if(!text && !m.quoted) return conn.reply(m.chat, `*[❗] USO APROPIADO*\n\n*┯┷*\n*┠≽ ${usedPrefix}daradmin @tag*\n*┠≽ ${usedPrefix}darpoder -> responder a un mensaje*\n*┷┯*`, m)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*[ ⚠️ ] El número ingresado es incorrecto, por favor ingrese el número correcto*`, m)
+if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*[ ⚠️ ] 𝚃𝚑𝚎 𝚗𝚞𝚖𝚋𝚎𝚛 𝚎𝚗𝚝𝚎𝚛𝚎𝚍 𝚒𝚜 𝚒𝚗𝚌𝚘𝚛𝚛𝚎𝚌𝚝, 𝚙𝚕𝚎𝚊𝚜𝚎 𝚎𝚗𝚝𝚎𝚛 𝚝𝚑𝚎 𝚌𝚘𝚛𝚛𝚎𝚌𝚝 𝚗𝚞𝚖𝚋𝚎𝚛*`, m)
 	
 try {
 if(text) {
@@ -21,7 +21,7 @@ var user = number + '@s.whatsapp.net'
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-conn.reply(m.chat, `*[ ✅ ] ÓRDENES RECIBIDAS*`, m)
+conn.reply(m.chat, `*[ ✅ ] 𝙾𝚁𝙳𝙴𝚁𝚂 𝚁𝙴𝙲𝙴𝙸𝚅𝙴𝙳*`, m)
 }}
 handler.help = ['*593xxx*','*@usuario*','*responder chat*'].map(v => 'promote ' + v)
 handler.tags = ['group']
