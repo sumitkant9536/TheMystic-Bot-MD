0e2e9803e0ebdcb3022fad53366c9b7ade2abf67
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-11 22:47:53 +0530

    Update owner-getmsg.js

--- plugins/owner-getmsg.js
+++ plugins/owner-getmsg.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, command, usedPrefix, text }) => {
 let which = command.replace(/ver/i, '')
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙰𝚁 *${usedPrefix}list${which}* 𝙿𝙰𝚁𝙰 𝚅𝙴𝚁 𝙻𝙰 𝙻𝙸𝚂𝚃𝙰*`
+if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚄𝚂𝙰𝚁 *${usedPrefix}list${which}* 𝚃𝙾 𝚅𝙸𝙴𝚆 𝚃𝙷𝙴 𝙻𝙸𝚂𝚃*`
 let msgs = global.db.data.msgs
-if (!text in msgs) throw `*[❗𝐈𝐍𝐅𝐎❗] '${text}' 𝙽𝙾 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙰𝙳𝙾 𝙴𝙽 𝙻𝙰 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴𝚂*`
+if (!text in msgs) throw `*[❗𝐈𝐍𝐅𝐎❗] '${text}' 𝙽𝙾𝚃 𝚁𝙴𝙶𝙸𝚂𝚃𝙴𝚁𝙴𝙳 𝙸𝙽 𝚃𝙷𝙴 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 𝙻𝙸𝚂𝚃*`
 let _m = await conn.serializeM(msgs[text])
 await _m.copyNForward(m.chat, true)
 }
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map(v => 'get' + v + ' <text>')
 handler.tags = ['database']
-handler.command = /^ver(vn|msg|video|audio|img|sticker)$/
+handler.command = /^get(vn|msg|video|audio|img|sticker)$/
 handler.rowner = true
 export default handler
