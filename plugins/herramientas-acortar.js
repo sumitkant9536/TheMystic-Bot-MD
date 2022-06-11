23214403df376675b90c25d620ad098bbba10fb3
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-11 15:27:05 +0530

    Update herramientas-acortar.js

--- plugins/herramientas-acortar.js
+++ plugins/herramientas-acortar.js
@@ -1,13 +1,13 @@
 import axiostal from "axios"
 import fetch from 'node-fetch'
 let handler = async(m, { conn, text, xteamkey }) => {
-if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝚄𝚁𝙻 𝙴𝙻 𝙲𝚄𝙰𝙻 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝙾𝚁𝚃𝙰𝚁*'
+if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙽𝚃𝙴𝚁 𝙰 𝙻𝙸𝙽𝙺/𝚄𝚁𝙻 𝚆𝙷𝙸𝙲𝙷 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃 𝚃𝙾 𝚂𝙷𝙾𝚁𝚃𝙴𝙽*'
 let json = await (await fetch(`https://api.xteam.xyz/shorturl/tinyurl?url=${text}&apikey=cb15ed422c71a2fb`)).json()
 if (!json.status) throw json
-let hasil = `*𝙻𝙸𝙽𝙺 𝙰𝙲𝙾𝚁𝚃𝙰𝙳𝙾 𝙲𝙾𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴!!*\n\n*𝙻𝙸𝙽𝙺 𝙰𝙽𝚃𝙴𝚁𝙸𝙾𝚁:*\n${text}\n*𝙻𝙸𝙽𝙺 𝙰𝙲𝙾𝚁𝚃𝙰𝙳𝙾:*\n*${json.result}*`.trim()   
+let hasil = `*𝙻𝙸𝙽𝙺 𝚂𝙷𝙾𝚁𝚃𝙴𝙽𝙴𝙳 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙻𝚈!!*\n\n*𝙿𝚁𝙴𝚅𝙸𝙾𝚄𝚂 𝙻𝙸𝙽𝙺:*\n${text}\n*𝚂𝙷𝙾𝚁𝚃𝙴𝙽𝙴𝙳 𝙻𝙸𝙽𝙺:*\n*${json.result}*`.trim()   
 m.reply(hasil)
 }
-handler.help = ['tinyurl','acortar'].map(v => v + ' <link>')
+handler.help = ['short'].map(v => v + ' <link>')
 handler.tags = ['tools']
 handler.command = /^(tinyurl|short|acortar|corto)$/i
 handler.fail = null
