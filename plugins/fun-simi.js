4be7c810c4f93dbc13426beb8721e65ca9aa3365
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-11 12:45:54 +0530

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
-if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾 𝙿𝙰𝚁𝙰 𝙷𝙰𝙱𝙻𝙰𝚁 𝙲𝙾𝙽 𝚂𝙸𝙼𝚂𝙸𝙼𝙸 𝙾 𝙴𝙻 𝙱𝙾𝚃*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} Hola bot*`
+if (!text) throw `*[❗] 𝙴𝙽𝚃𝙴𝚁 𝙰 𝚃𝙴𝚇𝚃 𝚃𝙾 𝚃𝙰𝙻𝙺 𝚃𝙾 𝚂𝙸𝙼𝚂𝙸𝙼𝙸 𝙾𝚁 𝚃𝙷𝙴 𝙱𝙾𝚃*\n\n*𝙴𝚇𝙰𝙼𝙿𝙻𝙴: ${usedPrefix + command} Hello bot*`
 let res = await fetch(`https://api-sv2.simsimi.net/v2/?text=${text}&lc=es`)
 let json = await res.json()
 let tes = json.success.replace('simsimi', 'simsimi').replace('Simsimi', 'Simsimi').replace('sim simi', 'sim simi')
