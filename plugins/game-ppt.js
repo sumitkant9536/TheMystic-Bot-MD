ecf16a2f24c36ba7ea975e726b64abe9381e0c7a
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-11 13:28:09 +0530

    Update game-ppt.js

--- plugins/game-ppt.js
+++ plugins/game-ppt.js
@@ -1,56 +1,56 @@
 let handler = async (m, { text, usedPrefix }) => {
-let INTROJUEGO = `*𝙷𝙾𝙻𝙰 👋🏻, 𝙴𝚂𝚃𝙰𝚂 𝙹𝚄𝙶𝙰𝙽𝙳𝙾 𝙰 "𝙿𝙸𝙴𝙳𝚁𝙰, 𝙿𝙰𝙿𝙴𝙻 𝙾 𝚃𝙸𝙹𝙴𝚁𝙰"*\n\n*𝙾𝙿𝙲𝙸𝙾𝙽𝙴𝚂 𝙳𝙸𝚂𝙿𝙾𝙽𝙸𝙱𝙻𝙴𝚂:*\n*piedra, papel o tijera*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix}ppt tijera*\n\n*- 𝚄𝚂𝙴 𝙻𝙰𝚂 𝙾𝙿𝙲𝙸𝙾𝙽𝙴𝚂 𝙴𝙽 𝙼𝙸𝙽𝚄𝚂𝙲𝚄𝙻𝙰𝚂 𝙲𝙾𝙼𝙾 𝚂𝙴 𝙸𝙽𝙳𝙸𝙲𝙰 𝙴𝙽 𝙴𝙻 𝙴𝙹𝙴𝙼𝙿𝙻𝙾*\n\n*𝘉𝘺 𝘛𝘩𝘦 𝘔𝘺𝘴𝘵𝘪𝘤 - 𝘉𝘰𝘵*`
+let INTROJUEGO = `*𝙷𝙴𝙻𝙻𝙾 👋🏻, 𝚈𝙾𝚄 𝙰𝚁𝙴 𝙿𝙻𝙰𝚈𝙸𝙽𝙶 "𝚁𝙾𝙲𝙺, 𝙿𝙰𝙿𝙴𝚁 𝙾𝚁 𝚂𝙲𝙸𝚂𝚂𝙾𝚁𝚂"*\n\n*𝙰𝚅𝙰𝙸𝙻𝙰𝙱𝙻𝙴 𝙾𝙿𝚃𝙸𝙾𝙽𝚂:*\n*stone, paper o scissor*\n\n*𝙴𝚇𝙰𝙼𝙿𝙻𝙴:*\n*${usedPrefix}ppt scissor*\n\n*- 𝚄𝚂𝙴 𝚃𝙷𝙴 𝙾𝙿𝚃𝙸𝙾𝙽𝚂 𝙸𝙽 𝙼𝙸𝙽𝚄𝚂𝙲𝚄𝙻𝙴𝚂 𝙰𝚂 𝚂𝙷𝙾𝚆𝙽 𝙸𝙽 𝚃𝙷𝙴 𝙴𝚇𝙰𝙼𝙿𝙻𝙴*\n\n*𝘉𝘺 𝘛𝘩𝘦 𝘔𝘺𝘴𝘵𝘪𝘤 - 𝘉𝘰𝘵*`
 if (!text) throw INTROJUEGO
 var astro = Math.random()
 if (astro < 0.34) {
-astro = 'piedra' 
+astro = 'stone' 
 } else if (astro > 0.34 && astro < 0.67) {
-astro = 'tijera' 
+astro = 'scissor' 
 } else {
-astro = 'papel'
+astro = 'paper'
 }
 if (text == astro) {
 global.db.data.users[m.sender].exp += 500
-m.reply(`*🔰 Empate!*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Puntos +500 XP*`)
-} else if (text == 'papel') {
-if (astro == 'piedra') {
+m.reply(`*🔰 Tie!*\n\n*👉🏻 You: ${text}*\n*👉🏻 The bot: ${astro}*\n*🎁 points +500 XP*`)
+} else if (text == 'paper') {
+if (astro == 'stone') {
 global.db.data.users[m.sender].exp += 1000
-m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Puntos +1000 XP*`)
+m.reply(`*🥳 You win! 🎉*\n\n*👉🏻 You: ${text}*\n*👉🏻 The bot: ${astro}*\n*🎁 points +1000 XP*`)
 } else {
 global.db.data.users[m.sender].exp -= 300
-m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*\n*❌ Puntos -300 XP*`)
+m.reply(`*☠️ You lose! ❌*\n\n*👉🏻 You: ${text}*\n*👉🏻 The bot: ${astro}*\n*❌ points -300 XP*`)
 }
-} else if (text == 'tijera') {
-if (astro == 'papel') {
+} else if (text == 'scissor') {
+if (astro == 'paper') {
 global.db.data.users[m.sender].exp += 1000
-m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Puntos +1000 XP*`)
+m.reply(`*🥳 You win! 🎉*\n\n*👉🏻 You: ${text}*\n*👉🏻 The bot: ${astro}*\n*🎁 points +1000 XP*`)
 } else {
 global.db.data.users[m.sender].exp -= 300
-m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*\n*❌ Puntos -300 XP*`)
+m.reply(`*☠️ You lose! ❌*\n\n*👉🏻 You: ${text}*\n*👉🏻 The bot: ${astro}*\n*❌ points -300 XP*`)
 }
-} else if (text == 'tijera') {
-if (astro == 'papel') {
+} else if (text == 'scissor') {
+if (astro == 'paper') {
 global.db.data.users[m.sender].exp += 1000
-m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Puntos +1000 XP*`)
+m.reply(`*🥳 You win! 🎉*\n\n*👉🏻 You: ${text}*\n*👉🏻 The bot: ${astro}*\n*🎁 points +1000 XP*`)
 } else {
 global.db.data.users[m.sender].exp -= 300
-m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*\n*❌ Puntos -300 XP*`)
+m.reply(`*☠️ You lose! ❌*\n\n*👉🏻 You: ${text}*\n*👉🏻 The bot: ${astro}*\n*❌ points -300 XP*`)
 }
-} else if (text == 'papel') {
-if (astro == 'piedra') {
+} else if (text == 'paper') {
+if (astro == 'stone') {
 global.db.data.users[m.sender].exp += 1000
-m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Puntos +1000 XP*`)
+m.reply(`*🥳 You win! 🎉*\n\n*👉🏻 You: ${text}*\n*👉🏻 The bot: ${astro}*\n*🎁 points +1000 XP*`)
 } else {
 global.db.data.users[m.sender].exp -= 300
-m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*\n*❌ Puntos -300 XP*`)
+m.reply(`*☠️ You lose! ❌*\n\n*👉🏻 You: ${text}*\n*👉🏻 The bot: ${astro}*\n*❌ points -300 XP*`)
 }
-} else if (text == 'piedra') {
-if (astro == 'tijera') {
+} else if (text == 'stone') {
+if (astro == 'scissor') {
 global.db.data.users[m.sender].exp += 1000
-m.reply(`*🥳 Tú ganas! 🎉*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*\n*🎁 Puntos +1000 XP*`)
+m.reply(`*🥳 You win! 🎉*\n\n*👉🏻 You: ${text}*\n*👉🏻 The bot: ${astro}*\n*🎁 points +1000 XP*`)
 } else {
 global.db.data.users[m.sender].exp -= 300
-m.reply(`*☠️ Tú pierdes! ❌*\n\n*👉🏻 Usted: ${text}*\n*👉🏻 El Bot: ${astro}*\n*❌ Puntos -300 XP*`)
+m.reply(`*☠️ You lose! ❌*\n\n*👉🏻 You: ${text}*\n*👉🏻 The bot: ${astro}*\n*❌ points -300 XP*`)
 }
 } else {
 throw INTROJUEGO
@@ -59,3 +59,4 @@ handler.help = ['ppt']
 handler.tags = ['games']
 handler.command = /^(ppt)$/i
 export default handler
+-
