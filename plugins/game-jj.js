135719ae68e0aacf7e032ed8e9874eb211cf80a0
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-13 02:15:11 +0530

    Create game-jj.js

---
+++ plugins/game-jj.js
@@ -0,0 +1,15 @@
+let fetch from 'node-fetch'
+let handler = async (m, { conn, command }) => {
+  let res = await fetch(`https://api.kaysa.xyz/api/${command}?apikey=KaysaS`)
+  if (!res.ok) throw 'Error Website server down'
+  let json = await res.json()
+  if (!json.video) throw 'Error!'
+   conn.sendFile(m.chat, json.video, 'meme.mp4', `© ꜱɪʀɪᴜꜱ`, m)
+}
+handler.help = ['jjfreefire', 'jjml', 'jjpubg', 'jjanime']
+handler.tags = ['fun']
+handler.command = /^(jjfreefire|jjml|jjpubg|jjanime)$/i
+
+handler.limit = true
+
+export default handler
