8cbe313e34f1e5e8f16086419f1ae6387053d63d
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-12 23:11:09 +0530

    Create xvideod.js

---
+++ plugins/xvideod.js
@@ -0,0 +1,19 @@
+//by AsuKidal
+
+let fetch from 'node-fetch'
+let fs from 'fs'
+let handler = async(m, { conn, usedPrefix, text, command }) => {
+    if (!text) throw `Please enter URL as text \n\nExample : ${usedPrefix + command} https://www.xvideos.com/video25381801/beautiful_sisters_have_been_tentacled`
+    let res = await fetch(`https://api-xcoders.xyz/api/download/xvideos?url=${text}&apikey=xcoders`)
+    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let json = await res.json()
+   await conn.sendFile(m.chat, json.result.url, 'bokp.mp4', `Title : ${json.result.title}\nViews : ${json.result.views}\nVote : ${json.result.vote}\nLike : ${json.result.like_count}\nDislike : ${json.result.dislike_count}\n\n\nkeyword : ${json.result.keyword}`, m)
+}
+handler.help = ['xvideod *link*']
+handler.tags = ['nsfw']
+handler.command = /^xvideod$/i
+
+handler.private = false
+handler.limit = 1
+
+export default handler
