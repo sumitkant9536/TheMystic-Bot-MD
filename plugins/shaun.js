82510937c100c0cef7889d1cba49bb0636ddeee5
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-12 16:24:12 +0530

    Create shaun.js

---
+++ plugins/shaun.js
@@ -0,0 +1,14 @@
+import uploadImage from '../lib/uploadImage.js'
+let handler = async (m) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'reply to the picture!'
+  let media = await q.download()
+  let url = await uploadImage(media)
+  await conn.sendFile(m.chat, global.API('xteam', '/videomaker/shaunthesheep', { url }, 'APIKEY'), 'shaunthesheep.mp4', '', m)
+}
+handler.help = ['shaunthesheep']
+handler.tags = ['videomaker']
+
+handler.command = /^shaunthesheep$/i
+export default handler
