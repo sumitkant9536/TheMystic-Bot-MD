054793700bd04e7cc887046272998424e91e4ba5
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-13 13:10:51 +0530

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command, args }) => {
 const fetch = (await import('node-fetch')).default
 let full = /f$/i.test(command)
 let text = args.join` `
-if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝙾 𝚃𝙴𝙼𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*', m)
+if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙽𝚃𝙴𝚁 𝚃𝙷𝙴 𝚃𝙴𝚇𝚃 𝙾𝚁 𝚃𝙾𝙿𝙸𝙲 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃 𝚃𝙾 𝚂𝙴𝙰𝚁𝙲𝙷 𝙵𝙾𝚁*', m)
 let url = 'https://google.com/search?q=' + encodeURIComponent(text)
 let search = await googleIt(text)
 let msg = search.articles.map(({
