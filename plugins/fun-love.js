fc8a2c3bfcd8c59a75b3e0ebe21da088396468e2
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-13 12:02:38 +0530

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, command, text }) => {
 conn.reply(m.chat, `
 *❤️❤️ 𝙻𝙾𝚅𝙴 𝙼𝙴𝚃𝙴𝚁 ❤️❤️*
 *The love of ${text} for you is from* *${Math.floor(Math.random() * 100)}%* *of a 100%*
-*You should ask him to be your girlfriend ?*
+*You should ask him/her to be your girlfriend/boyfriend ?*
 `.trim(), m, m.mentionedJid ? {
 contextInfo: {
 mentionedJid: m.mentionedJid
