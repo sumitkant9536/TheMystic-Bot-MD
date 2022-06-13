0621e11f2c24589869a7604e7010b7f7325d083b
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-13 15:45:53 +0530

    Update downloader-tiktokstalk.js

--- plugins/downloader-tiktokstalk.js
+++ plugins/downloader-tiktokstalk.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async(m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚃𝙸𝙺𝚃𝙾𝙺*', m)
+if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃 𝚃𝙷𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳 𝙿𝙻𝚄𝚂 𝚃𝙷𝙴 𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴 𝙾𝙵 𝙰 𝚃𝙸𝙺𝚃𝙾𝙺 𝚄𝚂𝙴𝚁*', m)
 try {
 let res = await fetch(`https://api.lolhuman.xyz/api/stalktiktok/${text}?apikey=9b817532fadff8fc7cb86862`)
 let res2 = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=9b817532fadff8fc7cb86862`
@@ -9,17 +9,17 @@ if (res.status !== 200) throw await res.text()
 if (!json.status) throw json
 let thumb = await (await fetch(json.result.user_picture)).buffer()
 let Mystic = `
-*𝚄𝚂𝚄𝙰𝚁𝙸𝙾:* ${json.result.username}
-*𝙽𝙾𝙼𝙱𝚁𝙴:* ${json.result.nickname}
-*𝚂𝙴𝙶𝚄𝙸𝙳𝙾𝚁𝙴𝚂:* ${json.result.followers}
-*𝚂𝙴𝙶𝚄𝙸𝙳𝙾𝚂:* ${json.result.followings}
+*𝚄𝚂𝙴𝚁:* ${json.result.username}
+*𝙽𝙰𝙼𝙴:* ${json.result.nickname}
+*𝙵𝙾𝙻𝙻𝙾𝚆𝙴𝚁𝚂:* ${json.result.followers}
+*𝙵𝙾𝙻𝙻𝙾𝚆𝙴𝙳:* ${json.result.followings}
 *𝙻𝙸𝙺𝙴𝚂:* ${json.result.likes}
 *𝚅𝙸𝙳𝙴𝙾𝚂:* ${json.result.video}
-*𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽:* ${json.result.bio}
+*𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝚃𝙸𝙾𝙽:* ${json.result.bio}
 `.trim()
 conn.sendFile(m.chat, res2, 'error.jpg', Mystic, m, false)
 } catch (e) {
-throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙽𝙾 𝚂𝙴 𝙴𝙲𝙾𝙽𝚃𝚁𝙾 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝙶𝚁𝙴𝚂𝙰𝙳𝙾*'
+throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝚃𝙷𝙴 𝙴𝙽𝚃𝙴𝚁𝙴𝙳 𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴 𝚆𝙰𝚂 𝙽𝙾𝚃 𝙲𝙷𝙴𝙲𝙺𝙴𝙳*'
 }}
 handler.help = ['tiktokstalk'].map(v => v + ' <username>')
 handler.tags = ['stalk']
