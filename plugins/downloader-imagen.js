97d21fab5347a87749a479ad7ae06f5924eb7038
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-13 15:20:13 +0530

    Update downloader-imagen.js

--- plugins/downloader-imagen.js
+++ plugins/downloader-imagen.js
@@ -4,10 +4,10 @@ if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
 const res = await googleImage(text)
 let image = res.getRandom()
 let link = image
-conn.sendHydrated(m.chat, `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}
-🌎 *𝙱𝚄𝙲𝙰𝙳𝙾𝚁:* Google
+conn.sendHydrated(m.chat, `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃 𝙾𝙵:* ${text}
+🌎 *𝚂𝙴𝙴𝙺𝙴𝚁:* Google
 `, author, link, link, '🔗 𝚄𝚁𝙻', null, null, [
-['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/imagen ${text}`]
+['🔄 𝙽𝙴𝚇𝚃 🔄', `/imagen ${text}`]
 ], m)
 }
 handler.help = ['gimage <query>', 'imagen <query>']
