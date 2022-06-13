1b840999a7d77894365a3c98ecac7644e57792cd
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-13 15:13:16 +0530

    Update downloader-igstory.js

--- plugins/downloader-igstory.js
+++ plugins/downloader-igstory.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import hx from 'hxz-api' 
 let handler = async (m, { conn, args, usedPrefix, command, text}) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} luisitocomunica*`        
+if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙽𝚃𝙴𝚁 𝚃𝙷𝙴 𝙽𝙰𝙼𝙴 𝙾𝙵 𝙰𝙽 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼 𝚄𝚂𝙴𝚁*\n\n*𝙴𝚇𝙰𝙼𝙿𝙻𝙴:*\n*${usedPrefix + command} war_sumit02*`        
 hx.igstory(text).then(async (result) => {
 for (let i of result.medias) {
 if (i.url.includes("mp4")) {            
