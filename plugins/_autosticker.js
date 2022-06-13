fec559410c51a4b5fcb426623d237626bde516f5
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-13 12:19:08 +0530

    Update _autosticker.js

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -16,7 +16,7 @@ let img = await q.download?.()
 if (!img) return
 stiker = await sticker(img, false, packname, author)
 } else if (/video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await this.sendButton(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 𝟽 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*', wm, [['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁', '/disable autosticker']], m)
+if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return await this.sendButton(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝚃𝙷𝙴 𝚅𝙸𝙳𝙴𝙾 𝙲𝙰𝙽𝙽𝙾𝚃 𝙻𝙰𝚂𝚃 𝙼𝙾𝚁𝙴 𝚃𝙷𝙰𝙽 𝟽 𝚂𝙴𝙲𝙾𝙽𝙳𝚂*', wm, [['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝚄𝚃𝙾𝚂𝚃𝙸𝙲𝙺𝙴𝚁', '/disable autosticker']], m)
 let img = await q.download()
 if (!img) return
 stiker = await sticker(img, false, packname, author)
