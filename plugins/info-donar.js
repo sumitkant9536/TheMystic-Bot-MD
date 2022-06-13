5438c410dca8788dfad10988b21e45cf3aa7867a
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-13 12:01:50 +0530

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -4,30 +4,25 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 let name = await conn.getName(m.sender)
 let donar =`
 *┏ ┅ ━━━━━━━━━ ┅ ━*
-*┇          「 𝐃𝐎𝐍𝐀𝐑 」*
+*┇          「 𝐃𝐎𝐍𝐀𝐓𝐄 」*
 *┣ ┅ ━━━━━━━━━ ┅ ━*
-*┃ 𝙷𝙾𝙻𝙰 ${name} 💙*
+*┃ 𝙷𝙴𝙻𝙻𝙾 ${name} 💙*
 *┃*
-*┃ 👉🏻 𝙰𝚀𝚄𝙸 𝙰𝙻𝙶𝚄𝙽𝙾𝚂 𝙳𝙰𝚃𝙾𝚂*
-*┃ 𝙿𝙾𝚁 𝚂𝙸 𝙶𝚄𝚂𝚃𝙰𝚂 𝙰𝙿𝙾𝚈𝙰𝚁 :𝟹*
+*┃ 👉🏻 𝙷𝙴𝚁𝙴 𝙰𝚁𝙴 𝚂𝙾𝙼𝙴 𝙵𝙰𝙲𝚃𝚂*
+*┃ 𝙸𝙽 𝙲𝙰𝚂𝙴 𝚈𝙾𝚄 𝙻𝙸𝙺𝙴 𝚃𝙾 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 :𝟹*
 *┃*
-*┃ ➤ 𝙲𝙻𝙰𝚅𝙴:* 
-*┃ 646180192175788481*
-*┃ ➤ 𝙱𝙰𝙽𝙲𝙾: STP* 
-*┃ ➤ 𝙱𝙴𝙽𝙴𝙵𝙸𝙲𝙸𝙰𝚁𝙸𝙾:* 
-*┃ BRUNO SOBRINO* 
-*┃ ➤ 𝙲𝙾𝙽𝙲𝙴𝙿𝚃𝙾: APOYO*  
-*┃ ➤ 𝙿𝙰𝚈𝙿𝙰𝙻: https://www.paypal.me/TheShadowBrokers133*
+*┃
+*┃ ➤ 𝙿𝙰𝚈𝚃𝙼: https://www.paypal.me/TheShadowBrokers133*
 *┃*
-*┃ 👉🏻 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙰𝙼𝙴 𝚂𝙸* 
-*┃ 𝙽𝙴𝙲𝙴𝚂𝙸𝚃𝙰𝚂 𝙼𝙰𝚂*
-*┃ 𝙳𝙰𝚃𝙾𝚂 𝚈 𝙿𝙰𝚁𝙰* 
-*┃ 𝙰𝙶𝚁𝙰𝙳𝙴𝙲𝙴𝚁𝚃𝙴 <𝟹*
-*┃ wa.me/5219996125657*
+*┃ 👉🏻 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙼𝙴 𝚈𝙴𝚂* 
+*┃ 𝚈𝙾𝚄 𝙽𝙴𝙴𝙳 𝙼𝙾𝚁𝙴*
+*┃ 𝙳𝙰𝚃𝙰 𝙰𝙽𝙳 𝚃𝙾* 
+*┃ 𝚃𝙷𝙰𝙽𝙺 𝚈𝙾𝚄 <𝟹*
+*┃ wa.me/919758523260*
 *┗ ┅ ━━━━━━━━━ ┅ ━*
 `.trim()
-conn.sendHydrated(m.chat, donar, wm, null, 'https://www.paypal.me/TheShadowBrokers133', '𝙿𝙰𝚈𝙿𝙰𝙻', null, null, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)}
-handler.help = ['donasi']
+conn.sendHydrated(m.chat, donar, wm, null, 'https://www.paypal.me/TheShadowBrokers133', '𝙿𝙰𝚈𝚃𝙼', null, null, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)}
+handler.help = ['donate']
 handler.tags = ['info']
 handler.command = /^dona(te|si)|donar|apoyar$/i
 export default handler
